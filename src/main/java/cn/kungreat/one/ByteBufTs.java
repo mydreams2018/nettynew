ccfd4741a3ca9ba0f730724b86cef952ebaf68db
kungreat2021 <kungreat2021@outlook.com>
2022-03-10 18:47:03 +0800

    CompositeByteBuf

--- src/main/java/cn/kungreat/one/ByteBufTs.java
+++ src/main/java/cn/kungreat/one/ByteBufTs.java
@@ -1,13 +1,17 @@
 package cn.kungreat.one;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class ByteBufTs {
@@ -28,6 +32,8 @@ public class ByteBufTs {
         }
         //writerIndex 指针位置
         System.out.println(buffer.writerIndex());//12
+
+        System.out.println("arrayOffset"+buffer.arrayOffset()); //0
         //设置 writerIndex 指针位置
         buffer.writerIndex(3);
         System.out.println(buffer.writableBytes()); //capacity() - writerIndex;
@@ -45,10 +51,40 @@ public class ByteBufTs {
         //设置当前指针
         buffer.readerIndex(0);
         //readerIndex  有增加
-        for(int x=0;x<10;x++){
+        for(int x=0;x<buffer.readableBytes();x++){
             System.out.println(buffer.readByte());
         }
 
-        Unpooled.copiedBuffer("dfsdfsdfsdf", Charset.forName("UTF-8"));
+        System.out.println("-------------------------");
+        Unpooled.buffer(128);
+        Unpooled.directBuffer(128);
+        ByteBuf wrappedBuffer = Unpooled.wrappedBuffer(new byte[128], new byte[256]);
+        ByteBuf copiedBuffer  = Unpooled.copiedBuffer(ByteBuffer.allocate(128));
+        ByteBuf byteBuf1 = Unpooled.copiedBuffer("dfsdfsdfsdf", Charset.forName("UTF-8"));
+        ByteBuf byteBuf2 = Unpooled.copiedBuffer("dfsdfsdfsdf", Charset.forName("UTF-8"));
+        //CompositeByteBuf 可以把需要合并的bytebuf 组合起来，对外提供统一的readindex和writerindex
+
+        //将多个缓冲区显示为单个合并缓冲区的虚拟缓冲区。  添加几个数量后合并成一个新的数组 默认16
+        CompositeByteBuf compositeBuffer = Unpooled.compositeBuffer();
+        compositeBuffer.addComponent(copiedBuffer);
+        //true 增加写入指针位置  不然写入指针不动
+        compositeBuffer.addComponent(true,byteBuf1);
+        //从指定位置插入 不会删除旧元素 位置会顺移
+        compositeBuffer.addComponent(true,byteBuf2);
+
+        //合并后的总数量
+        System.out.println(compositeBuffer.capacity());
+        //删除 components 数组的第N个索引   byteBufs.removeComponent(1);
+        System.out.println(compositeBuffer.capacity());
+        for(ByteBuf buf:compositeBuffer){
+            System.out.println(buf);
+        }
+        //可以读的总字节
+        System.out.println(compositeBuffer.readableBytes());
+        System.out.println(compositeBuffer.readerIndex());
+        byte[] bt = new byte[compositeBuffer.readableBytes()];
+        //有风险, 如果添加数据的顺序有问题 或者 添加了空的数据 就会写入空的到bt
+        CompositeByteBuf bytes = compositeBuffer.getBytes(compositeBuffer.readerIndex(), bt);
+        System.out.println(Arrays.toString(bt));
     }
 }
